9a24fb06961720d0d2b5445a0be4f7636e03772f
nazneen636 <nazneennahar636@gmail.com>
2024-11-09 12:32:47 +0600

    product add

--- my-project/src/Page/Product.jsx
+++ my-project/src/Page/Product.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Container from '../Layer/Container'
+import React from "react";
+import Container from "../Layer/Container";
 
 const Product = () => {
   return (
     <div>
       <Container>
-        <div className='h-[3818 px] bg-[#FFFF]'>
-jjjjjjjjjjjjjjj
-        </div>
+        <div className="h-[3818 px] bg-[#FFFF]">jjjjjjjjjjjjjjj</div>
+        <p>kfjfkfkk</p>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
